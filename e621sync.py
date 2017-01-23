a45ec5422606dc7d8756821f08dedc5520fe238b
Matthew Davey <github@project-2501.net>
2017-01-23 21:28:23 +1100

    Added common tags to config files and made tags arrays of strings

--- e621sync.py
+++ e621sync.py
@@ -15,6 +15,8 @@ def run_rule(name, rule):
         print('Making directory: {}'.format(rule['download_directory']))
         os.makedirs(rule['download_directory'])
 
+    print('Building file list', end='')
+
     # Build complete list of all data first
     # For now, just be reasonable and don't build large searches?
     # TODO: Add --everything command line argument to re-check and fetch everything, else just sync recent stuff?
@@ -26,7 +28,7 @@ def run_rule(name, rule):
         if len(items) == 0:
             break
 
-        print('Found {} items'.format(len(items)))
+        print('.', end='')
 
         for item in items:
             filename = rule['download_directory'] + str(item['id']) + '_' + item['md5'] + '.' + item['file_ext']
@@ -38,27 +40,52 @@ def run_rule(name, rule):
             if not os.path.exists(filename):
                 download_list.append({'id': item['id'], 'url': item['file_url'], 'dest': filename})
 
-    print('Found {} new items to fetch'.format(len(download_list)))
+    count_total_items = len(download_list)
+    count_completed_items = 0
+    print('')
+    print('Found {} new items to fetch'.format(count_total_items))
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=CONFIG['max_workers']) as executor:
         futures = {executor.submit(download, item['url'], item['dest']): item for item in download_list}
 
         for future in concurrent.futures.as_completed(futures):
+            count_completed_items += 1
             item = futures[future]
             try:
                 bytes_written = future.result()
-                print('Download {} (size: {})'.format(item['dest'], bytes_written))
+                print('[{}/{}]  Downloaded {} (size: {})'.format(count_completed_items,
+                                                                 count_total_items,
+                                                                 item['dest'], bytes_written))
             except Exception as e:
-                print('Download {} failed: {}'.format(item['dest'], e))
+                print('[{}/{}]  Download {} failed: {}'.format(count_completed_items,
+                                                               count_total_items,
+                                                               item['dest'], e))
 
 
-def get_list(tag_string, before_id=None):
-    args = {'tags': tag_string, 'limit': CONFIG['list_limit']}
+def get_list(tags, before_id=None):
+    all_tags = []
+    all_tags.extend(tags)
+
+    # Apply common tags
+    all_tags.extend(CONFIG['common_tags'])
+
+    # TODO: While a good idea, this eats into our 6 tag maximum
+    # Always order by image id  (fixes issue with sets being in order added to set)
+    # all_tags.append('order:-id')
+
+    args = {'tags': ' '.join(all_tags), 'limit': CONFIG['list_limit']}
+
+    # If None, just get the latest
     if before_id is not None:
         args['before_id'] = before_id
 
     r = requests.get('https://e621.net/post/index.json', args, headers={'User-Agent': USER_AGENT}, timeout=30)
-    return r.json()
+    json = r.json()
+
+    if 'success' in json and json['success'] is False:
+        raise Exception('Error getting list: {}'.format(json['reason']))
+
+    return json
 
 
 def download(url, filename):
@@ -69,7 +96,5 @@ def download(url, filename):
 
 
 if __name__ == '__main__':
-    conf = toml.load(open('./config.toml'))
-
-    for rule_name, rule in dict.items(conf['rules']):
-        run_rule(rule_name, rule)
+    for rule_name in CONFIG['rules']:
+        run_rule(rule_name, CONFIG['rules'][rule_name])
--- readme.md
+++ readme.md
@@ -2,12 +2,17 @@
 
 Really simple python project to download files from e621.net based on tags
 
-Downloaded file name is hard coded to `"{id}_{md5}.{ext}"`
+
+## features
+
+  * Multi-threaded downloads
+  * Supports multiple rules each with their own tags and download locations
+  * That's it...
 
 
 ## requirements
 
- * Python 3  (tested using Python 3.4)
+ * Python 3  (tested using Python 3.4/3.5 on Windows and Linux)
 
 
 ## install
@@ -18,14 +23,40 @@ Downloaded file name is hard coded to `"{id}_{md5}.{ext}"`
     
 ## configuration
 
-All settings live in `config.toml` and are documented there
+All settings live in `config.toml` and are documented there.
+
+Lines beginning with `#` are comments and are not read by the program. 
 
 To create a new rule simple follow this template:
 
-    [rule.NewStuff]
-    tags = "this -that"
+    [rule.UniqueTagName]
+    tags = ["this", "that"]
     download_directory = "./downloads/new_stuff/"
+
+The rule name (e.g. `UniqueTagName`) itself isn't important, it should just be unique.  You can include spaces in name 
+too e.g. `[rule."Proper Sentence"]`.
+
+The file format is in TOML, if you want more information about the syntax see:
+
+ * https://github.com/toml-lang/toml
+
 
 ## running
 
     python e621sync.py
+
+    
+## bugs
+
+1. Downloaded file names are hard coded to `"{id}_{md5}.{ext}"`
+
+2. When searching via a set, the default order returned is by the date the item was added to the set.  You will need to 
+include the tag 'order:-id' to correctly discover all items.  e.g.
+
+
+    [rule."A Set"]
+    tags = ["set:myset", "order:-id"]
+    download_directory = "./downloads/my_set/"
+    
+3. Every run all items are re-checked and re-downloaded if missing.  There is no id, or date checks to only update new
+files.
